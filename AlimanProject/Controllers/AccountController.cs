83b494d42c224a33f1e59f769b482476e58fb084
unknown known <misterandrax@outlook.com>
2020-09-19 13:04:16 +0600

    Added video controller and views

---
+++ AlimanProject/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlimanProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult AccountChats()
+        {
+            return View();
+        }
+        public IActionResult AccountContact()
+        {
+            return View();
+        }
+        public IActionResult AccountEdit()
+        {
+            return View();
+        }
+    }
+}
